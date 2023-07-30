59f8b60e7640a979741e3bb079f18e76650944b3
Vasharaladeva <51453264+Vasharaladeva@users.noreply.github.com>
2023-07-30 00:00:11 -0400

    Add files via upload
    node app

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+// const estudianteController = require('../controller/estudianteController');
+const  nuevoEstudianteCtrl  = require('../controller/nuevoEstudianteCtrl');
+
+module.exports = function() {
+
+
+
+    // estudiante rute
+
+    router.get('/estudiante',nuevoEstudianteCtrl.mostrarEstudiante)
+    //muestra un estudiante en espesifico ID
+    router.get('/estudiante/:id', nuevoEstudianteCtrl.mostrarEstudianteId)
+
+    router.post('/estudiante',
+    nuevoEstudianteCtrl.subirArchivo,
+    nuevoEstudianteCtrl.nuevoEstudiante
+    );
+
+    router.put('/estudiante/:id/cambiar-estatus', nuevoEstudianteCtrl.cambiarEstatusEstudianteID);
+
+
+    return router;
+
+}
+
